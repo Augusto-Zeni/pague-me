efc7e601fd41c706b15705ef6447228d98c3d837
Augusto-Zeni <augustozeni955@gmail.com>
2025-06-11 11:29:18 -0300

    feat: add notifications screen

--- app/home/notifications/index.tsx
+++ app/home/notifications/index.tsx
@@ -1,18 +1,98 @@
 import { Text } from '@/src/components/Text'
+import { TransactionCard } from '@/src/components/TransactionCard'
+import { colors } from '@/src/styles/colors'
 import { globalStyles } from '@/src/styles/globalStyles'
-import { StyleSheet, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native'
 
 export default function notifications() {
   return (
-    <View style={[globalStyles.container, styles.container]}>
-      <Text>teste notifcations</Text>
-    </View>
+    <SafeAreaView style={[globalStyles.container, styles.container]}>
+      <ScrollView>
+        <View style={styles.content}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.title}>Notificações</Text>
+          </View>
+
+          <View style={styles.cardsContainer}>
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Maria transferiu R$ 50,00 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Carlos transferiu R$ 8,75 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Hora de acertar as contas!</Text>
+              <Text style={styles.contentTransactionCard}>O mês está acabando. Que tal revisar e quitar suas despesas no grupo &quot;Despesas do Mês&quot;?</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Maria transferiu R$ 50,00 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Carlos transferiu R$ 8,75 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Hora de acertar as contas!</Text>
+              <Text style={styles.contentTransactionCard}>O mês está acabando. Que tal revisar e quitar suas despesas no grupo &quot;Despesas do Mês&quot;?</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Maria transferiu R$ 50,00 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Você recebeu um pagamento</Text>
+              <Text style={styles.contentTransactionCard}>Carlos transferiu R$ 8,75 para você no grupo &quot;Churrasco de Sábado&quot;.</Text>
+            </TransactionCard>
+
+            <TransactionCard>
+              <Text style={styles.titleTransactionCard}>Hora de acertar as contas!</Text>
+              <Text style={styles.contentTransactionCard}>O mês está acabando. Que tal revisar e quitar suas despesas no grupo &quot;Despesas do Mês&quot;?</Text>
+            </TransactionCard>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    paddingTop: 24,
+    paddingHorizontal: 24,
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    lineHeight: 36,
+    color: colors.blue,
+  },
+  cardsContainer: {
+    marginTop: 12,
+  },
+  titleTransactionCard: {
+    fontSize: 22,
+    color: colors.blue,
+  },
+  contentTransactionCard: {
+    fontSize: 14,
+    marginTop: 3,
   },
 })
